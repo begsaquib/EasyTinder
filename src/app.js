db9aec5dbeffc14b1bc5a9d363db5ff3d17de7ed
begsaquib <saquibbeg4444@gmail.com>
2024-10-19 20:40:53 +0530

    Exploring get,delete,post

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({ name:"Saquib",lastname:"Beg"})
+})
+app.post("/user",(req,res)=>{
+    res.send("Data Succesfully saved")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data Succesfully Deleted")
+})
 
+//ORDER OF THE ROUTES MATTER A LOT!!!!
 app.use("/test", (req,res) => {
   res.send("only test");
 });
-app.use("/home", (req,res) => {
-  res.send("only home page");
-});
-app.use("/", (req, res) => {
-    res.send("only slash"); 
-  });
+
 app.listen(7777, () => {
   console.log("Server is running perfectly");
 });
