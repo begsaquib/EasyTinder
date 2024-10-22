d34dff2bb254a367ad7a1225341faee8698728e0
begsaquib <saquibbeg4444@gmail.com>
2024-10-22 17:51:09 +0530

    middleware and routes handling

--- src/app.js
+++ src/app.js
@@ -1,26 +1,21 @@
 const express = require("express");
+const { auth } = require("./middleware/auth");
 
 const app = express();
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    console.log("logges 1");
+app.use("/admin",auth)
 
-    res.send("response 1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("logges 2");
-   // res.send("response 2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("logges 3");
-   // res.send("response 3");
-    //next();
-  }
-);
+app.get("/user",auth,(req,res)=>{
+  res.send("user data send")
+})
+
+app.get("/admin/getdata",(req,res,next)=>{
+  res.send("authorised user data recieved")
+  
+})
+app.get("/admin/deletedata",(req,res)=>{
+  res.send("authorised user data is deleted")
+})
 
 app.listen(7777, () => {
   console.log("Server is running perfectly");
